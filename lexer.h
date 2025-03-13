c0bafb443e80d17594db850e88801ea4b1ffe0cd
Gyan <gyaneshroybhowmik04@gmail.com>
2025-03-13 15:46:16 +0530

    added getTokenName to lexer.h and printParseTree to parser.c

--- lexer.h
+++ lexer.h
@@ -11,6 +11,7 @@ FILE *getStream(FILE *fp);
 tokenInfo getNextToken(twinBuffer B, FILE *fp);
 void removeComments(char *testcaseFile, char *cleanFile);
 vector* getAllTokens(FILE* fp);
+char* getTokenName(TOKEN_TYPE type); 
 
 
 #endif
--- parser.c
+++ parser.c
@@ -123,7 +123,38 @@ parseTree *parseInputSourceCode(table T, FirstFollow F, grammar G,
     }
   }
 }
+void printParseTree(parseTree *PT, FILE *outfile){
+      if (PT == NULL) {
+        return;
+    }
+    if (PT->no_of_children != 0 && PT->children[0] != NULL){
+        printParseTree(PT->children[0], outfile);
+    }
+    if (outfile != NULL){
+        if (PT != NULL) {
+          fprintf(outfile, "%-20s", (PT->t.lexeme != NULL) ? PT->t.lexeme : "----");
+          fprintf(outfile, "%-20d", (PT->t.line != -1) ? PT->t.line : -1);
+          fprintf(outfile, "%-20d", PT->t.type);
 
+          if ((PT->t.type == TK_RNUM) || (PT->t.type == TK_NUM)) {
+              fprintf(outfile, "%-20s", PT->t.lexeme);
+          } else {
+              fprintf(outfile, "%-20s", "----");
+          }
+          // if (PT->parent) aayega
+          } else {
+              fprintf(outfile, "%-20s%-20s%-20s%-20s", "----", "----", "----", "----");
+          }
+          fprintf(outfile, "%-20s", (PT->no_of_children == 0) ? "YES" : "NO");
+          fprintf(outfile, "%-20s", getTokenName(PT->t.type)); 
+          fprintf(outfile, "\n");
+      }
+    for(int i = 1; i < PT->no_of_children; i++){
+        if (PT->children[i]!=NULL){
+        printParseTree(PT->children[i],outfile);
+        }
+    }
+}
 int main() {
   FILE *fp = fopen("Lexer_Test/t2.txt", "r");
   if (fp == NULL) {
@@ -155,15 +186,26 @@ int main() {
   // for(int i=0;i<rule.element_count;i++){
   //   printf("%d ",rule.elements[i].var);
   // }
-
   parseTree *root = parseInputSourceCode(T, F, G, input);
-
   if (root == NULL) {
     printf("Error: Parsing failed\n");
     fclose(fp);
     return 1;
   }
-
+  // printf("here0");
   fclose(fp);
+  // printf("here");
+  FILE *outfile = fopen("parse.txt","w");
+  if (outfile == NULL) {
+    printf("Error: Unable to open file\n");
+    return 1;
+  }
+  if (root == NULL) {
+    printf("Error: Parsing failed. Cannot print parse tree.\n");
+    fclose(outfile);
+    return 1;
+}
+  printParseTree(root,outfile);
+  fclose(outfile);
   return 0;
 }
